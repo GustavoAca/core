46ccebf1b0972301439bfdfb226e659eae2e8713
Gustavo Acácio <gustavo.acacio@attus.ai>
2024-11-20 13:56:03 -0300

    ajustes em workflow

--- src/main/java/com/glaiss/core/controller/RestExceptionHandler.java
+++ src/main/java/com/glaiss/core/controller/RestExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 public class RestExceptionHandler {
 
